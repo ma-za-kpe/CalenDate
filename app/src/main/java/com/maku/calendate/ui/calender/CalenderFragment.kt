8a64c746c4fdf1858267a08e55b9cb723c05309f
partyParty01 <makpalyy@gmail.com>
2020-06-11 10:36:37 -0700

    add calenderview

--- app/src/main/java/com/maku/calendate/ui/calender/CalenderFragment.kt
+++ app/src/main/java/com/maku/calendate/ui/calender/CalenderFragment.kt
@@ -1,14 +1,17 @@
 package com.maku.calendate.ui.calender
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.maku.calendate.R
+import java.util.*
+
 
 class CalenderFragment : Fragment() {
 
@@ -19,8 +22,8 @@ class CalenderFragment : Fragment() {
     ): View? {
 
         val root = inflater.inflate(R.layout.fragment_calender, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
 
         return root
     }
+
 }
