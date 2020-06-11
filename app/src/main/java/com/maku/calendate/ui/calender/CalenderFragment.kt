db13ccbef851ab55328a9672568839677c6a4bd5
partyParty01 <makpalyy@gmail.com>
2020-06-11 14:06:04 -0700

    added logo

--- app/src/main/java/com/maku/calendate/ui/calender/CalenderFragment.kt
+++ app/src/main/java/com/maku/calendate/ui/calender/CalenderFragment.kt
@@ -1,16 +1,12 @@
 package com.maku.calendate.ui.calender
 
-import android.app.DatePickerDialog
-import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.DatePicker
-import android.widget.TextView
+import android.widget.CalendarView
 import androidx.fragment.app.Fragment
 import com.maku.calendate.R
-import java.util.*
 
 
 class CalenderFragment : Fragment() {
@@ -23,7 +19,17 @@ class CalenderFragment : Fragment() {
 
         val root = inflater.inflate(R.layout.fragment_calender, container, false)
 
+        getSelectedDate();
+
         return root
+    }
+
+    private fun getSelectedDate() {
+//        val simpleCalendarView = findViewById(R.id.simpleCalendarView) as CalendarView // get the reference of CalendarView
+
+//        val selectedDate =
+//            simpleCalendarView.date // get selected date in milliseconds
+
     }
 
 }
